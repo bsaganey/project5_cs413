7bba06e5051c11ce2b6489c70c732c1fe4599962
bsaganey <bs366@nau.edu>
2015-04-29 22:08:28 -0700

    fix the boundaries in level one

--- src/Root.hx
+++ src/Root.hx
@@ -57,44 +57,34 @@ class Root extends Sprite{
 		var Bound12 = new Rectangle(384,706,272,33);
 		var Bound13 = new Rectangle(385,724,272,54);
 		
-		
-		//level one Bounds
-			var LBound1 = new Rectangle(4.4287, 3.32152 ,21.0363, 458.37);
-			var LBound2  =new Rectangle(4.4287 ,519.265, 19.9291, 197.077);
-			var LBound3 = new Rectangle(31.0009 ,679.805, 1240.04 ,21.0363);
-			var LBound4 = new Rectangle(1248.89, 5.53587 ,24.3578, 673.162);
-			var LBound5 = new Rectangle(633.2152, 4.4287, 1214.57 ,8.8574);
-			var LBound6 = new Rectangle(1044.07 ,17.7148 ,19.9291 ,75.2879);
-			var LBound7 = new Rectangle(1042.96 ,166.076 ,204.827 ,23.2507);
-			var LBound8 = new Rectangle(1044.07 ,193.756 ,21.0363 ,378.654);
-			var LBound9 = new Rectangle(1075.07 ,438.441 ,101.86 ,21.0363);
-			var LBound10 = new Rectangle(1153.68 ,261.293 ,24.3578 ,171.612);
-			var LBound11 = new Rectangle(548.051 ,18.822 ,22.1435 ,71.9663);
-			var LBound12 = new Rectangle(577.945 ,69.752 ,132.861 ,23.2507);
-			var LBound13 = new Rectangle(276.794 ,167.183 ,677.591 ,21.0363);
-			var LBound14 = new Rectangle(403.012 ,53.1444 ,21.0363 ,107.396);
-			var LBound15 = new Rectangle(117.36 ,85.2524 ,18.822 ,136.182);
-			var LBound16 = new Rectangle(141.718 ,87.4668 ,152.79 ,18.822);
-			var LBound17 = new Rectangle(276.794 ,114.039 ,22.1435 ,45.3942);
-			var LBound18 = new Rectangle(484.942 ,195.97 ,24.3578 ,368.689);
-			var LBound19 = new Rectangle(403.012 ,566.873 ,195.97 ,21.0363);
-			var LBound20 = new Rectangle(580.159 ,311.116 ,19.9291 ,252.436);
-			var LBound21 = new Rectangle(605.624 ,311.116 ,187.112 ,17.7148);
-			var LBound22 = new Rectangle(6773.915, 246.9 ,18.822 ,54.2516);
-			var LBound23 = new Rectangle(802.702, 249.114, 117.36 ,17.7148);
-			var LBound24 = new Rectangle(901.24 ,274.579 ,19.9291 ,166.076);
-			var LBound25 = new Rectangle(708.592 ,421.833 ,191.541 ,23.2507);
-			var LBound26 = new Rectangle(707.484 ,452.834 ,23.2507 ,219.221);
+		var Bound14 = new Rectangle(0,0,0,0);
+		var Bound15 = new Rectangle(0,0,0,0);
+		var Bound16 = new Rectangle(0,0,0,0);
+		var Bound17 = new Rectangle(0,0,0,0);
+		var Bound18 = new Rectangle(0,0,0,0);
+		var Bound19 = new Rectangle(0,0,0,0);
+		var Bound20 = new Rectangle(0,0,0,0);
+		var Bound21 = new Rectangle(0,0,0,0);
+		var Bound22 = new Rectangle(0,0,0,0);
+		var Bound23 = new Rectangle(0,0,0,0);
+		var Bound24 = new Rectangle(0,0,0,0);
+		var Bound25 = new Rectangle(0,0,0,0);
+		var Bound26 = new Rectangle(0,0,0,0);
 			
-			var LBound27 = new Rectangle(837.024 ,550.266 ,202.613 ,19.9291);
-			var LBound28 = new Rectangle(400.797 ,277.901, 23.2507, 283.437);
-			var LBound29 = new Rectangle(7275.686, 276.794 ,121.789 ,22.1435);
-			var LBound30 = new Rectangle(276.794 ,306.687 ,21.0363 ,106.289);
-			var LBound31 = new Rectangle(277.901 ,503.764 ,19.9291 ,87.4668);
-			var LBound32 = new Rectangle(31.0009 ,567.981 ,242.471 ,22.1435);
-			var LBound33 = new Rectangle(116.253 ,293.401 ,21.0363 ,151.683);
-			var LBound34 = new Rectangle(29.8937 ,426.262 ,85.2524 ,18.822);
-			var LBound35 = new Rectangle(819.309 ,69.752 ,222.542 ,25.465);
+		var Bound27 = new Rectangle(0,0,0,0);
+		var Bound28 = new Rectangle(0,0,0,0);
+		var Bound29 = new Rectangle(0,0,0,0);
+		var Bound30 = new Rectangle(0,0,0,0);
+		var Bound31 = new Rectangle(0,0,0,0);
+		var Bound32 = new Rectangle(0,0,0,0);
+		var Bound33 = new Rectangle(0,0,0,0);
+		var Bound34 = new Rectangle(0,0,0,0);
+		var Bound35 = new Rectangle(0,0,0,0);
+		
+		var levelonePortal = new Rectangle(0,0,0,0);
+
+		
+		
 
     public function new() {
         super();
@@ -154,8 +144,8 @@ class Root extends Sprite{
                         onComplete: function() {
                             removeChildAt(0, true);
 							ninja = new Image(Root.assets.getTexture("ninja"));
-							ninja.x = 600;
-							ninja.y = 360;
+							ninja.x = 506;
+							ninja.y = 695;
 							ninja.pivotX = ninja.width  * 0.5;
 							ninja.pivotY = ninja.height * 0.5;
 		
@@ -176,29 +166,35 @@ class Root extends Sprite{
 								if (event.keyCode == Keyboard.LEFT) {
 									ninja.rotation = (3*Math.PI/2);
 									ninja.x -= 10;
-									//leveloneBoundsR(ninja);
+									leveloneBoundsL(ninja);
 									
 								}
 								
 								if (event.keyCode == Keyboard.RIGHT) {
 									ninja.rotation = (Math.PI/2);
 									ninja.x += 10;
-									//leveloneBoundsL(ninja);
+									leveloneBoundsR(ninja);
 								}
 								if (event.keyCode == Keyboard.UP) {
 									ninja.rotation = (0);
 									ninja.y -= 10;
-									//leveloneBoundsUP(ninja);
+									leveloneBoundsUP(ninja);
 								}
 								if (event.keyCode == Keyboard.DOWN) {
 									ninja.rotation = (Math.PI);
 									ninja.y += 10;
-									//leveloneBoundsD(ninja);
+									leveloneBoundsD(ninja);
 								}
-							level1RB(ninja);
+								
+							if(checkCollision(ninja,levelonePortal)){
+								ninja.x = 25;
+								ninja.y = 490;
+							}
+							
 							doorEnter(ninja);
 							});
-							//stage.addEventListener(Event.ENTER_FRAME, movecam);
+							
+							
                         }
         });
 	
@@ -292,18 +288,18 @@ class Root extends Sprite{
 	}
 	
 	public function leveloneBoundsR(Image){
-			if(checkCollision(Image,LBound1)||checkCollision(Image,LBound2)||checkCollision(Image,LBound3)||
-			checkCollision(Image,LBound4)||checkCollision(Image,LBound5)||checkCollision(Image,LBound6)||
-			checkCollision(Image,LBound7)||checkCollision(Image,LBound8)||checkCollision(Image,LBound9)||
-			checkCollision(Image,LBound10)||checkCollision(Image,LBound11)||checkCollision(Image,LBound12)||
-			checkCollision(Image,LBound13)||checkCollision(Image,LBound14)||checkCollision(Image,LBound15)||
-			checkCollision(Image,LBound16)||checkCollision(Image,LBound17)||checkCollision(Image,LBound18)||
-			checkCollision(Image,LBound19)||checkCollision(Image,LBound20)||checkCollision(Image,LBound21)||
-			checkCollision(Image,LBound22)||checkCollision(Image,LBound23)||checkCollision(Image,LBound24)||
-			checkCollision(Image,LBound25)||checkCollision(Image,LBound26)||checkCollision(Image,LBound27)||
-			checkCollision(Image,LBound28)||checkCollision(Image,LBound29)||checkCollision(Image,LBound30)||
-			checkCollision(Image,LBound31)||checkCollision(Image,LBound32)||checkCollision(Image,LBound33)||
-			checkCollision(Image,LBound34)||checkCollision(Image,LBound35)){
+			if(checkCollision(Image,Bound1)||checkCollision(Image,Bound2)||checkCollision(Image,Bound3)||
+			checkCollision(Image,Bound4)||checkCollision(Image,Bound5)||checkCollision(Image,Bound6)||
+			checkCollision(Image,Bound7)||checkCollision(Image,Bound8)||checkCollision(Image,Bound9)||
+			checkCollision(Image,Bound10)||checkCollision(Image,Bound11)||checkCollision(Image,Bound12)||
+			checkCollision(Image,Bound13)||checkCollision(Image,Bound14)||checkCollision(Image,Bound15)||
+			checkCollision(Image,Bound16)||checkCollision(Image,Bound17)||checkCollision(Image,Bound18)||
+			checkCollision(Image,Bound19)||checkCollision(Image,Bound20)||checkCollision(Image,Bound21)||
+			checkCollision(Image,Bound22)||checkCollision(Image,Bound23)||checkCollision(Image,Bound24)||
+			checkCollision(Image,Bound25)||checkCollision(Image,Bound26)||checkCollision(Image,Bound27)||
+			checkCollision(Image,Bound28)||checkCollision(Image,Bound29)||checkCollision(Image,Bound30)||
+			checkCollision(Image,Bound31)||checkCollision(Image,Bound32)||checkCollision(Image,Bound33)||
+			checkCollision(Image,Bound34)||checkCollision(Image,Bound35)){
 				Image.x -= 10;
 			
 			}
@@ -311,18 +307,18 @@ class Root extends Sprite{
 	
 	public function leveloneBoundsUP(Image){
 			
-			if(checkCollision(Image,LBound1)||checkCollision(Image,LBound2)||checkCollision(Image,LBound3)||
-			checkCollision(Image,LBound4)||checkCollision(Image,LBound5)||checkCollision(Image,LBound6)||
-			checkCollision(Image,LBound7)||checkCollision(Image,LBound8)||checkCollision(Image,LBound9)||
-			checkCollision(Image,LBound10)||checkCollision(Image,LBound11)||checkCollision(Image,LBound12)||
-			checkCollision(Image,LBound13)||checkCollision(Image,LBound14)||checkCollision(Image,LBound15)||
-			checkCollision(Image,LBound16)||checkCollision(Image,LBound17)||checkCollision(Image,LBound18)||
-			checkCollision(Image,LBound19)||checkCollision(Image,LBound20)||checkCollision(Image,LBound21)||
-			checkCollision(Image,LBound22)||checkCollision(Image,LBound23)||checkCollision(Image,LBound24)||
-			checkCollision(Image,LBound25)||checkCollision(Image,LBound26)||checkCollision(Image,LBound27)||
-			checkCollision(Image,LBound28)||checkCollision(Image,LBound29)||checkCollision(Image,LBound30)||
-			checkCollision(Image,LBound31)||checkCollision(Image,LBound32)||checkCollision(Image,LBound33)||
-			checkCollision(Image,LBound34)||checkCollision(Image,LBound35)){
+			if(checkCollision(Image,Bound1)||checkCollision(Image,Bound2)||checkCollision(Image,Bound3)||
+			checkCollision(Image,Bound4)||checkCollision(Image,Bound5)||checkCollision(Image,Bound6)||
+			checkCollision(Image,Bound7)||checkCollision(Image,Bound8)||checkCollision(Image,Bound9)||
+			checkCollision(Image,Bound10)||checkCollision(Image,Bound11)||checkCollision(Image,Bound12)||
+			checkCollision(Image,Bound13)||checkCollision(Image,Bound14)||checkCollision(Image,Bound15)||
+			checkCollision(Image,Bound16)||checkCollision(Image,Bound17)||checkCollision(Image,Bound18)||
+			checkCollision(Image,Bound19)||checkCollision(Image,Bound20)||checkCollision(Image,Bound21)||
+			checkCollision(Image,Bound22)||checkCollision(Image,Bound23)||checkCollision(Image,Bound24)||
+			checkCollision(Image,Bound25)||checkCollision(Image,Bound26)||checkCollision(Image,Bound27)||
+			checkCollision(Image,Bound28)||checkCollision(Image,Bound29)||checkCollision(Image,Bound30)||
+			checkCollision(Image,Bound31)||checkCollision(Image,Bound32)||checkCollision(Image,Bound33)||
+			checkCollision(Image,Bound34)||checkCollision(Image,Bound35)){
 				Image.y += 10;
 			}
 	}
@@ -330,18 +326,18 @@ class Root extends Sprite{
 	public function leveloneBoundsL(Image){
 					
 
-						if(checkCollision(Image,LBound1)||checkCollision(Image,LBound2)||checkCollision(Image,LBound3)||
-			checkCollision(Image,LBound4)||checkCollision(Image,LBound5)||checkCollision(Image,LBound6)||
-			checkCollision(Image,LBound7)||checkCollision(Image,LBound8)||checkCollision(Image,LBound9)||
-			checkCollision(Image,LBound10)||checkCollision(Image,LBound11)||checkCollision(Image,LBound12)||
-			checkCollision(Image,LBound13)||checkCollision(Image,LBound14)||checkCollision(Image,LBound15)||
-			checkCollision(Image,LBound16)||checkCollision(Image,LBound17)||checkCollision(Image,LBound18)||
-			checkCollision(Image,LBound19)||checkCollision(Image,LBound20)||checkCollision(Image,LBound21)||
-			checkCollision(Image,LBound22)||checkCollision(Image,LBound23)||checkCollision(Image,LBound24)||
-			checkCollision(Image,LBound25)||checkCollision(Image,LBound26)||checkCollision(Image,LBound27)||
-			checkCollision(Image,LBound28)||checkCollision(Image,LBound29)||checkCollision(Image,LBound30)||
-			checkCollision(Image,LBound31)||checkCollision(Image,LBound32)||checkCollision(Image,LBound33)||
-			checkCollision(Image,LBound34)||checkCollision(Image,LBound35)){
+						if(checkCollision(Image,Bound1)||checkCollision(Image,Bound2)||checkCollision(Image,Bound3)||
+			checkCollision(Image,Bound4)||checkCollision(Image,Bound5)||checkCollision(Image,Bound6)||
+			checkCollision(Image,Bound7)||checkCollision(Image,Bound8)||checkCollision(Image,Bound9)||
+			checkCollision(Image,Bound10)||checkCollision(Image,Bound11)||checkCollision(Image,Bound12)||
+			checkCollision(Image,Bound13)||checkCollision(Image,Bound14)||checkCollision(Image,Bound15)||
+			checkCollision(Image,Bound16)||checkCollision(Image,Bound17)||checkCollision(Image,Bound18)||
+			checkCollision(Image,Bound19)||checkCollision(Image,Bound20)||checkCollision(Image,Bound21)||
+			checkCollision(Image,Bound22)||checkCollision(Image,Bound23)||checkCollision(Image,Bound24)||
+			checkCollision(Image,Bound25)||checkCollision(Image,Bound26)||checkCollision(Image,Bound27)||
+			checkCollision(Image,Bound28)||checkCollision(Image,Bound29)||checkCollision(Image,Bound30)||
+			checkCollision(Image,Bound31)||checkCollision(Image,Bound32)||checkCollision(Image,Bound33)||
+			checkCollision(Image,Bound34)||checkCollision(Image,Bound35)){
 				Image.x += 10;
 			}
 
@@ -351,18 +347,18 @@ class Root extends Sprite{
 	
 	public function leveloneBoundsD(Image){
 	
-			if(checkCollision(Image,LBound1)||checkCollision(Image,LBound2)||checkCollision(Image,LBound3)||
-			checkCollision(Image,LBound4)||checkCollision(Image,LBound5)||checkCollision(Image,LBound6)||
-			checkCollision(Image,LBound7)||checkCollision(Image,LBound8)||checkCollision(Image,LBound9)||
-			checkCollision(Image,LBound10)||checkCollision(Image,LBound11)||checkCollision(Image,LBound12)||
-			checkCollision(Image,LBound13)||checkCollision(Image,LBound14)||checkCollision(Image,LBound15)||
-			checkCollision(Image,LBound16)||checkCollision(Image,LBound17)||checkCollision(Image,LBound18)||
-			checkCollision(Image,LBound19)||checkCollision(Image,LBound20)||checkCollision(Image,LBound21)||
-			checkCollision(Image,LBound22)||checkCollision(Image,LBound23)||checkCollision(Image,LBound24)||
-			checkCollision(Image,LBound25)||checkCollision(Image,LBound26)||checkCollision(Image,LBound27)||
-			checkCollision(Image,LBound28)||checkCollision(Image,LBound29)||checkCollision(Image,LBound30)||
-			checkCollision(Image,LBound31)||checkCollision(Image,LBound32)||checkCollision(Image,LBound33)||
-			checkCollision(Image,LBound34)||checkCollision(Image,LBound35)){
+			if(checkCollision(Image,Bound1)||checkCollision(Image,Bound2)||checkCollision(Image,Bound3)||
+			checkCollision(Image,Bound4)||checkCollision(Image,Bound5)||checkCollision(Image,Bound6)||
+			checkCollision(Image,Bound7)||checkCollision(Image,Bound8)||checkCollision(Image,Bound9)||
+			checkCollision(Image,Bound10)||checkCollision(Image,Bound11)||checkCollision(Image,Bound12)||
+			checkCollision(Image,Bound13)||checkCollision(Image,Bound14)||checkCollision(Image,Bound15)||
+			checkCollision(Image,Bound16)||checkCollision(Image,Bound17)||checkCollision(Image,Bound18)||
+			checkCollision(Image,Bound19)||checkCollision(Image,Bound20)||checkCollision(Image,Bound21)||
+			checkCollision(Image,Bound22)||checkCollision(Image,Bound23)||checkCollision(Image,Bound24)||
+			checkCollision(Image,Bound25)||checkCollision(Image,Bound26)||checkCollision(Image,Bound27)||
+			checkCollision(Image,Bound28)||checkCollision(Image,Bound29)||checkCollision(Image,Bound30)||
+			checkCollision(Image,Bound31)||checkCollision(Image,Bound32)||checkCollision(Image,Bound33)||
+			checkCollision(Image,Bound34)||checkCollision(Image,Bound35)){
 				Image.y -= 10;
 			}
 	}
@@ -379,32 +375,57 @@ class Root extends Sprite{
 		
 		if(checkCollision(Image, Door1)){
 			
-					//right bounds 
-				Bound1 = new Rectangle(0,0,0,0);
-				Bound2 = new Rectangle(0,0,0,0);
-				Bound3 = new Rectangle(0,0,0,0);
-				Bound4 = new Rectangle(0,0,0,0);
-				Bound5 = new Rectangle(0,0,0,0);
-		
-				//left bounds
-				Bound6 = new Rectangle(0,0,0,0);
-				Bound7 = new Rectangle(0,0,0,0);
-				Bound8 = new Rectangle(0,0,0,0);
-		
-				// top bounds
-				Bound9 = new Rectangle(0,0,0,0);
-				Bound10 = new Rectangle(0,0,0,0);
-		
-				// bottom bounds
-				Bound11 = new Rectangle(0,0,0,0);
-				Bound12 = new Rectangle(0,0,0,0);
-				Bound13 = new Rectangle(0,0,0,0);
 
-
+				
+						//level one Bounds
+				Bound1 = new Rectangle(4.4287, 3.32152 ,21.0363, 458.37);
+				Bound2  =new Rectangle(4.4287 ,519.265, 19.9291, 197.077);
+				Bound3 = new Rectangle(31.0009 ,679.805, 1240.04 ,21.0363);
+				Bound4 = new Rectangle(1248.89, 5.53587 ,24.3578, 673.162);
+				Bound5 = new Rectangle(633.2152, 4.4287, 1214.57 ,8.8574);
+				Bound6 = new Rectangle(1044.07 ,17.7148 ,19.9291 ,75.2879);
+				Bound7 = new Rectangle(1042.96 ,166.076 ,204.827 ,23.2507);
+				Bound8 = new Rectangle(1044.07 ,193.756 ,21.0363 ,378.654);
+				Bound9 = new Rectangle(1075.07 ,438.441 ,101.86 ,21.0363);
+				Bound10 = new Rectangle(1153.68 ,261.293 ,24.3578 ,171.612);
+				Bound11 = new Rectangle(548.051 ,18.822 ,22.1435 ,71.9663);
+				Bound12 = new Rectangle(577.945 ,69.752 ,132.861 ,23.2507);
+				Bound13 = new Rectangle(276.794 ,167.183 ,677.591 ,21.0363);
+				Bound14 = new Rectangle(403.012 ,53.1444 ,21.0363 ,107.396);
+				Bound15 = new Rectangle(117.36 ,85.2524 ,18.822 ,136.182);
+				Bound16 = new Rectangle(141.718 ,87.4668 ,152.79 ,18.822);
+				Bound17 = new Rectangle(276.794 ,114.039 ,22.1435 ,45.3942);
+				Bound18 = new Rectangle(484.942 ,195.97 ,24.3578 ,368.689);
+				Bound19 = new Rectangle(403.012 ,566.873 ,195.97 ,21.0363);
+				Bound20 = new Rectangle(580.159 ,311.116 ,19.9291 ,252.436);
+				Bound21 = new Rectangle(605.624 ,311.116 ,187.112 ,17.7148);
+				Bound22 = new Rectangle(6773.915, 246.9 ,18.822 ,54.2516);
+				Bound23 = new Rectangle(802.702, 249.114, 117.36 ,17.7148);
+				Bound24 = new Rectangle(901.24 ,274.579 ,19.9291 ,166.076);
+				Bound25 = new Rectangle(708.592 ,421.833 ,191.541 ,23.2507);
+				Bound26 = new Rectangle(707.484 ,452.834 ,23.2507 ,219.221);
 			
+				Bound27 = new Rectangle(837.024 ,550.266 ,202.613 ,19.9291);
+				Bound28 = new Rectangle(400.797 ,277.901, 23.2507, 283.437);
+				Bound29 = new Rectangle(7275.686, 276.794 ,121.789 ,22.1435);
+				Bound30 = new Rectangle(276.794 ,306.687 ,21.0363 ,106.289);
+				Bound31 = new Rectangle(277.901 ,503.764 ,19.9291 ,87.4668);
+				Bound32 = new Rectangle(31.0009 ,567.981 ,242.471 ,22.1435);
+				Bound33 = new Rectangle(116.253 ,293.401 ,21.0363 ,151.683);
+				Bound34 = new Rectangle(29.8937 ,426.262 ,85.2524 ,18.822);
+				Bound35 = new Rectangle(819.309 ,69.752 ,222.542 ,25.465);
+				
+				levelonePortal = new Rectangle(235,530,10,10);
+				level2Portal = new Rectangle(497,129,10,10);
+				level3Portal = new Rectangle(639,582,10,10);
+				level4Portal = new Rectangle(910,482,10,10);
+
 			removeChild(tmxMain);
 			levelone = new Tilemap(Root.assets, "levelone");
 			addChild(levelone);
+			Image.x = 25;
+			Image.y = 490;
+			
 			}
 			
 				if(checkCollision(Image, Door2)){
